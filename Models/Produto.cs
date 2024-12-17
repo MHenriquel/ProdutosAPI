f7876ad7a333325312f40e3505e6ca8b39b2a063
LAPTOP-42IGECQ3 <maykel.henriquel@gmail.com>
2024-12-16 22:04:18 -0300

    Ajustes de validação

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -1,10 +1,18 @@
-﻿namespace ProdutosAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProdutosAPI.Models
 {
     public class Produto
     {
         public int ProductID { get; set; }
+
+        [Required(ErrorMessage = "Nome Obrigatório")]
         public string Name { get; set; } = string.Empty;
+
+        [Range(1, int.MaxValue, ErrorMessage = "Preço precisa ser maior que {1}")]
         public decimal Price { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Estoque precisa ser maior que {1}")]
         public int StockQuantity { get; set; }
     }
 
